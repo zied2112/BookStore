8990311c241185d1e63b7ec2596a8da2257b0b84
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-09-26 20:03:37 +0000

    Add book display

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-console.log("Hello World");
+// console.log("Hello World");
+// Declare the button
+const submit =  document.querySelector('#submit-form');
+const displayContentContainer = document.querySelector('.show-added-books');
+const title =  document.querySelector('#title');
+const author = document.querySelector('#author');
+
+// window.onload = () => {
+    
+// }
+
+submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    displayContentContainer.style.display = 'block';
+    const book = `
+    <div class="user-input">
+        <h3 class="input-value">${title.value}</h3>
+        <h3 class="input-value">${author.value}</h3>
+        <button class="remove-button" onclick='removeDisplay()'>Remove</button>
+    </div>
+    `;
+
+    function removeDisplay() {
+        console.log()
+    }
+    
+    displayContentContainer.innerHTML +=book ;
+    // const btn = document.querySelector('.remove-button')
+    // btn.addEventListener("click", (event) => {
+    //     event.preventDefault()
+    //     console.log("remove")
+    // })
+})
+
+
+
+
+
