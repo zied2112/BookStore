af2d7a183404f33f0283889642230623a97f0f86
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-09-27 21:18:55 +0000

    Create class and methods

--- js/index.js
+++ js/index.js
@@ -3,22 +3,23 @@ const displayContentContainer = document.querySelector('.show-added-books');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 
-const bookArray = JSON.parse(localStorage.getItem('books')) || [];
-displayContentContainer.innerHTML = `
-    ${bookArray
-    .map((book, index) => `
-      <div class="user-input">
-        <h3 class="input-value">${book.title}</h3>
-        <p class="input-value">${book.author}</p>
-        <button class="delete-btn" id=${index}>Remove</button>
-      </div>
-    `)
-    .join('')}
-  `;
 
-window.addEventListener('load', () => {
-  submit.addEventListener('click', (event) => {
-    event.preventDefault();
+class AwesomeBook {
+  constructor (title, author) {
+    this.title = title;
+    this.author = author;
+  }
+
+}
+
+class NewAwesomeBook {
+  constructor (id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
+
+  addAwesomeBook() {
     if ((title.value === '') || (author.value === '')) {
       alert('Please put a title and author');
     } else {
@@ -36,12 +37,31 @@ window.addEventListener('load', () => {
     `;
       localStorage.setItem('books', JSON.stringify(bookArray));
     }
-  });
+  }
 
-  displayContentContainer.addEventListener('click', (event) => {
+  removeAwesomeBook() {
     bookArray.splice(event.target.id, 1);
     localStorage.setItem('books', JSON.stringify(bookArray));
     displayContentContainer.innerHTML = `
+      ${bookArray.map((book, index) => `
+        <div class="user-input">
+          <h3 class="input-value">${book.title}</h3>
+          <p class="input-value">${book.author}</p>
+          <button class="delete-btn" id=${index}>Remove</button>
+        </div>
+      `)
+      .join('')}
+     `;
+  }
+
+}
+
+const bookInput = new NewAwesomeBook;
+
+
+
+const bookArray = JSON.parse(localStorage.getItem('books')) || [];
+displayContentContainer.innerHTML = `
     ${bookArray
     .map((book, index) => `
       <div class="user-input">
@@ -52,5 +72,14 @@ window.addEventListener('load', () => {
     `)
     .join('')}
   `;
+
+  submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    bookInput.addAwesomeBook();
   });
-});
+
+  displayContentContainer.addEventListener('click', (event) => {
+  bookInput.removeAwesomeBook()
+  });
+// window.addEventListener('load', () => {
+// });
