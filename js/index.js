0b4a2ff8ec9a4ce0213f521b3abd26cabb769267
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-09-27 23:27:38 +0000

    Fix linter error

--- js/index.js
+++ js/index.js
@@ -3,61 +3,19 @@ const displayContentContainer = document.querySelector('.show-added-books');
 const title = document.querySelector('#title');
 const author = document.querySelector('#author');
 
-class NewAwesomeBook {
-  constructor(id, title, author) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
-  }
-
-  addAwesomeBook() {
-    if ((title.value === '') || (author.value === '')) {
-      alert('Please put a title and author');
-    } else {
-      bookArray.push({ title: title.value, author: author.value });
-      displayContentContainer.innerHTML = `
-      ${bookArray.map((book, index) => `
-        <div class="user-input">
-          <div class="input-div">
-          <h3 class="input-value">"${book.title}"</h3>
-          <h3 class="input-value"> by ${book.author}</h3>
-          </div>
-          <div class="detele-btn-div">
-          <button class="delete-btn" id=${index}>Remove</button>
-          </div>
-        </div>
-      `)
-    .join('')}
-    `;
-      localStorage.setItem('books', JSON.stringify(bookArray));
-    }
-  }
-
-  removeAwesomeBook() {
-    bookArray.splice(event.target.id, 1);
-    localStorage.setItem('books', JSON.stringify(bookArray));
-    displayContentContainer.innerHTML = `
-      ${bookArray.map((book, index) => `
-         <div class="user-input">
-          <div class="input-div">
-          <h3 class="input-value">"${book.title}"</h3>
-          <h3 class="input-value"> by ${book.author}</h3>
-          </div>
-          <div class="detele-btn-div">
-          <button class="delete-btn" id=${index}>Remove</button>
-          </div>
-        </div>
-      `)
-    .join('')}
-     `;
-  }
+function AwesomeBook(title, author) {
+  this.title = title;
+  this.author = author;
 }
 
-const bookInput = new NewAwesomeBook();
+class NewAwesomeBook {
+  constructor() {
+    this.bookArray = JSON.parse(localStorage.getItem('books')) || [];
+  }
 
-const bookArray = JSON.parse(localStorage.getItem('books')) || [];
-displayContentContainer.innerHTML = `
-    ${bookArray
+  displayBook() {
+    displayContentContainer.innerHTML = `
+    ${this.bookArray
     .map((book, index) => `
       <div class="user-input">
         <div class="input-div">
@@ -71,6 +29,29 @@ displayContentContainer.innerHTML = `
     `)
     .join('')}
   `;
+  }
+
+  addAwesomeBook() {
+    if ((title.value === '') || (author.value === '')) {
+      alert('Please put a title and author');
+    } else {
+      const newBook = new AwesomeBook(title.value, author.value);
+      this.bookArray.push(newBook);
+      localStorage.setItem('books', JSON.stringify(this.bookArray));
+      this.displayBook();
+    }
+  }
+
+  removeAwesomeBook(event) {
+    if (event.target.matches('button')) {
+      this.bookArray.splice(event.target.id, 1);
+      localStorage.setItem('books', JSON.stringify(this.bookArray));
+      this.displayBook();
+    }
+  }
+}
+
+const bookInput = new NewAwesomeBook();
 
 submit.addEventListener('click', (event) => {
   event.preventDefault();
@@ -78,7 +59,5 @@ submit.addEventListener('click', (event) => {
 });
 
 displayContentContainer.addEventListener('click', (event) => {
-  bookInput.removeAwesomeBook();
+  bookInput.removeAwesomeBook(event);
 });
-// window.addEventListener('load', () => {
-// });
