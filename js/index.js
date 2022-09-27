33e504e9cff16152301f552d86aefe50784c9d4f
Zied Ben Amor <zied-ci@hotmail.fr>
2022-09-27 15:37:24 +0200

    Updates in the books view

--- js/index.js
+++ js/index.js
@@ -1,59 +1,56 @@
 // console.log("Hello World");
 // Declare the button
 
-window.onload(function () {
-  const submit = document.querySelector('#submit-form');
-  var displayContentContainer = document.querySelector('.show-added-books');
-  const title = document.querySelector('#title');
-  const author = document.querySelector('#author');
+const submit = document.querySelector('#submit-form');
+const displayContentContainer = document.querySelector('.show-added-books');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
 
+const bookArray = JSON.parse(localStorage.getItem('books')) || [];
+displayContentContainer.innerHTML = `
+    ${bookArray
+    .map((book, index) => `
+      <div class="user-input">
+        <h3 class="input-value">${book.title}</h3>
+        <p class="input-value">${book.author}</p>
+        <button class="delete-btn" id=${index}>Remove</button>
+      </div>
+    `)
+    .join('')}
+  `;
 
-  const bookArray = [];
+window.addEventListener('load', () => {
   submit.addEventListener('click', (event) => {
     event.preventDefault();
-    displayContentContainer.style.display = 'block';
-    
-    const book = `
-    <div class="user-input">
-        <h3 class="input-value">${title.value}</h3>
-        <h3 class="input-value">${author.value}</h3>
-        <button class="remove-button" id="btn" >Remove</button>
-    </div>
-    `;
-    const bookData = {
-      title: title.value,
-      author: author.value
-    }
-    bookArray.push(bookData);
-    addLocalStorage(bookData);
-    displayContentContainer.innerHTML += book;
+    // displayContentContainer.style.display = "block";
+    bookArray.push({ title: title.value, author: author.value });
+    displayContentContainer.innerHTML = `
+    ${bookArray
+    .map((book, index) => `
+      <div class="user-input">
+        <h3 class="input-value">${book.title}</h3>
+        <p class="input-value">${book.author}</p>
+        <button class="delete-btn" id=${index}>Remove</button>
+      </div>
+    `)
+    .join('')}
+  `;
+    localStorage.setItem('books', JSON.stringify(bookArray));
+  });
 
-  })
-  localStorage.setItem("books", JSON.stringify([]));
-  function addLocalStorage(book) {
-    const items = JSON.parse(localStorage.getItem("books"));
-    items.push(book);
-    localStorage.setItem('books', JSON.stringify(items));
-  }
-
-  displayContentContainer
-    .querySelectorAll(".remove-button")
-    .forEach((item, index) => {
-      console.log(item);
-      item.addEventListener("click", (event) => {
-        displayContentContainer = document.querySelector(".show-added-books");
-        const itemsStorage = JSON.parse(localStorage.getItem("books"));
-        itemsStorage.splice(index, 1);
-        localStorage.setItem("books", JSON.stringify(itemsStorage));
-
-        displayContentContainer.removeChild(
-          displayContentContainer.children[index]
-        );
-      });
-    });
-
-
-})
-
-
-
+  displayContentContainer.addEventListener('click', (event) => {
+    bookArray.splice(event.target.id, 1);
+    localStorage.setItem('books', JSON.stringify(bookArray));
+    displayContentContainer.innerHTML = `
+    ${bookArray
+    .map((book, index) => `
+      <div class="user-input">
+        <h3 class="input-value">${book.title}</h3>
+        <p class="input-value">${book.author}</p>
+        <button class="delete-btn" id=${index}>Remove</button>
+      </div>
+    `)
+    .join('')}
+  `;
+  });
+});
